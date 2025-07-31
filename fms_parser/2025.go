76509c5fa0af67e080ef7e2fde8f069d62a92841
lethosor <lethosor@gmail.com>
2025-07-31 16:53:23 -0400

    calculate algaePoints

--- fms_parser/2025.go
+++ fms_parser/2025.go
@@ -459,6 +459,12 @@ func parseHTMLtoJSON2025(filename string, config FMSParseConfig) (map[string]int
 		breakdown[alliance]["autoCoralCount"] = auto_count
 		breakdown[alliance]["teleopCoralPoints"] = teleop_points
 		breakdown[alliance]["teleopCoralCount"] = teleop_count
+
+		if net_algae_count, ok := breakdown[alliance]["netAlgaeCount"].(int); ok {
+			if wall_algae_count, ok := breakdown[alliance]["wallAlgaeCount"].(int); ok {
+				breakdown[alliance]["algaePoints"] = 6*wall_algae_count + 4*net_algae_count
+			}
+		}
 	}
 
 	// // fields determined by coopertition:
