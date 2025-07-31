be05b6b0ecdb6a6c99d2b7ba9194908e5b4bf16d
lethosor <lethosor@gmail.com>
2025-07-31 03:15:52 -0400

    use go mod download

--- setup.sh
+++ setup.sh
@@ -3,7 +3,5 @@
 cd "$(dirname "$0")"
 . common.inc.sh
 
-run_cmd go get -u github.com/gorilla/mux
-run_cmd go get -u github.com/PuerkitoBio/goquery
-run_cmd go get -u github.com/go-test/deep
+run_cmd go mod download
 run_cmd yarn install
